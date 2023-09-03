11d43c69d50d12f3fea300fc9b9a6c9524e84f4e
sahil chaudhari <sahilchaudhari2405@gmail.com>
2023-09-04 01:39:32 +0530

    mobile input and button set

--- lib/loginPage.dart
+++ lib/loginPage.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 import 'package:reel_app/main.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
 class Login_page extends StatefulWidget {
   const Login_page({super.key});
@@ -16,8 +18,10 @@ class _Login_pageState extends State<Login_page> {
       body: SafeArea(
           child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
+          SizedBox(
+            height: mq.height * 0.11,
+          ),
           CircleAvatar(
             child: Image.asset(
               "asset/images/logo.png",
@@ -26,17 +30,35 @@ class _Login_pageState extends State<Login_page> {
             radius: mq.width * 0.25,
           ),
           SizedBox(
-            height: mq.height * 0.2,
+            height: mq.height * 0.15,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: IntlPhoneField(
+              decoration: InputDecoration(
+                labelText: 'Phone Number',
+                hintText: 'Phone Number',
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.red),
+                ),
+              ),
+              initialCountryCode: 'IN',
+              onChanged: (phone) {
+                print(phone.completeNumber);
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: ElevatedButton(
+                onPressed: () {},
+                child: Container(
+                  height: mq.height * 0.05,
+                  padding: EdgeInsets.all(8.0),
+                  alignment: Alignment.center,
+                  child: Text("NEXT"),
+                )),
           )
-           TextField(
-
-                          keyboardType: TextInputType.phone,
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            hintText: "Phone Number",
-                            
-                            ),
-                          ),
         ],
       )),
     );
